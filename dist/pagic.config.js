c76d79d75d5d6cb6c9a5bb2b4b49e75ee92f1a3a
Curtis Larson <quackware@gmail.com>
2021-12-24 15:58:15 -0500

    init

---
+++ dist/pagic.config.js
@@ -0,0 +1,6 @@
+import Pagic from "https://deno.land/x/pagic@v1.5.1/mod.js";
+const config = {
+    ...Pagic.defaultConfig,
+    title: "Tobey",
+};
+export default config;
---
+++ pagic.config.ts
@@ -0,0 +1,7 @@
+import Pagic, { PagicConfig } from "https://deno.land/x/pagic@v1.5.1/mod.ts";
+const config: PagicConfig = {
+  ...Pagic.defaultConfig,
+  title: "Tobey",
+};
+
+export default config;
